eb05db8dd17352e7f9820a51da547ae4882f2f18
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 00:41:25 +0530

    terrabox 1st file modified

--- instance.tf
+++ instance.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
     ami = "ami-04b4f1a9cf54c11d0"
     key_name = "yash-key.pem"
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     vpc_security_group_ids = [ "mysg" ]
     tags = {
         Name = "practice instancegit"
