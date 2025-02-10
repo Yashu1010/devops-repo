0119713b10ccfa2d13643e7316760a08c743fa4a
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 22:59:14 +0530

    terraform instance file modified

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,15 @@
+provider "aws" {
+    region = "us-east-1"    
+}
+resource "aws_instance" "myec2" {
+    ami = "ami-04b4f1a9cf54c11d0"
+    key_name = "yash-key.pem"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.pub_sub.id
+    tags = {
+        Name = "practice_instance"
+    }
+}
 resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/16"
 tags = {
