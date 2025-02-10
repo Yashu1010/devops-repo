9afe6e6de9d5da8b9ab398d8aca8d95f89e8235f
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 22:53:04 +0530

    terraform instance file modified

--- instance.tf
+++ instance.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "myec2" {
     ami = "ami-04b4f1a9cf54c11d0"
     key_name = "yash-key.pem"
     instance_type = "t2.micro"
-    security_groups = [ "mysg" ]
     subnet_id = aws_subnet.pub_sub.id
     tags = {
         Name = "practice instancegit"
