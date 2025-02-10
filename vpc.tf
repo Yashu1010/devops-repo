125be747e59be8040b3815e5f14430956148face
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 21:31:33 +0530

    terraform vpc file added

--- vpc.tf
+++ vpc.tf
@@ -25,3 +25,24 @@ resource "aws_subnet" "pvt_sub" {
   env = "dev"
 }
 }
+
+resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.myvpc.id
+  
+}
+
+resource "aws_route_table" "my_route" {
+    vpc_id = aws_vpc.myvpc.id
+
+    route {
+        cidr_block = "10.0.1.0/24"
+        gateway_id = aws_internet_gateway.my_igw
+    }
+  
+}
+
+resource "aws_route_table_association" "route_subnet" {
+    subnet_id = aws_subnet.pub_sub.id
+    route_table_id = aws_route_table.my_route.id
+  
+}
