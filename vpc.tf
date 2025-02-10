5d0c24a9760dfb7ce6f60a604336ac0c18b6f447
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 23:01:05 +0530

    terraform instance file modified

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "myec2" {
     ami = "ami-04b4f1a9cf54c11d0"
-    key_name = "yash-key.pem"
+    key_name = "yash-key"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.pub_sub.id
     tags = {
