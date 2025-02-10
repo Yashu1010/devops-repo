3fdfadc4c17ae556b3c441dd3f4939924d22d82b
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 22:36:44 +0530

    this is terraform vpc file

--- vpc.tf
+++ vpc.tf
@@ -35,7 +35,7 @@ resource "aws_route_table" "my_route" {
     vpc_id = aws_vpc.myvpc.id
 
     route {
-        cidr_block = "10.0.1.0/24"
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.my_igw.id
     }
   
