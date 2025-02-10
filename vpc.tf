1a43aaf42a3581ccc96b62cd103a7cd4a2166623
Yashu1010 <yashgandhi3614@gmail.com>
2025-02-10 19:55:37 +0530

    terraform vpc file added

---
+++ vpc.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+tags = {
+  Name = "myvpc"
+  env = "dev"
+}
+}
+
+resource "aws_subnet" "pub_sub" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.0.0/20"
+    map_public_ip_on_launch = true
+  tags = {
+  Name = "my_sub"
+  env = "dev"
+}
+}
+
+resource "aws_subnet" "pvt_sub" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.1.0/20"
+    map_public_ip_on_launch = false
+    tags = {
+  Name = "my_pvt_sub"
+  env = "dev"
+}
+}
